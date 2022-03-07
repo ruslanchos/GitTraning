3231c55134dafd50e6ef1ab84284457bb097c849
Ruslan Bityutskih <ruslanchos@gmail.com>
2022-03-07 19:40:29 +0300

    2-n commit

---
+++ Lession5_001.java
@@ -0,0 +1,8 @@
+public class Lession5_001 {
+    public static void main(String[] args) {
+
+//     /*  *//* int age = 15;
+//        if (age < 18)
+//           *//* System.out.println();*/
+    }
+}
